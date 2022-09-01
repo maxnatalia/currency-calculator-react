03a48cbf650002a7d9a04d3668f22efb74928228
Natalia Mazur-Zurek <nataliamazur1988@gmail.com>
2022-09-01 15:48:38 +0200

    removed curriencies.js

---
+++ src/Form/useExternalCurrencies.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export const useExternalCurrencies = () => {
+    const statusRequest = {
+        pending: "pending",
+        success: "success",
+        error: "error",
+    };
+
+    const [rates, setRates] = useState([]);
+    const [date, setDate] = useState();
+    const [status, setStatus] = useState(statusRequest.pending);
+
+    const src = "https://mmmapi.exchangerate.host/latest?base=PLN";
+
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                const response = await axios.get(src);
+                setDate(response.data.date);
+                setRates(response.data.rates);
+                setStatus(() => statusRequest.success);
+            } catch (error) {
+                setStatus(() => statusRequest.error);
+                console.error("Something bad happened!", error);
+            }
+        };
+        setTimeout(getData, 5000);
+    }); 
+
+    return { rates, date, status, statusRequest };
+};
